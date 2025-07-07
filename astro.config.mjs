9e23a85edc3a136ff9a365ebd67233138627d50b
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2025-07-07 09:20:32 +0900

    Create basic docs structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,49 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Hello, World!",
+          autogenerate: { directory: "guides/hello-world" },
+        },
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "guides/getting-started" },
+        },
+        {
+          label: "Core Concepts",
+          autogenerate: { directory: "guides/core-concepts" },
+        },
+        {
+          label: "Backend Adapters",
+          autogenerate: { directory: "guides/backend-adapters" },
+        },
+        {
+          label: "Advanced Usage",
+          autogenerate: { directory: "guides/advanced-usage" },
+        },
+        {
+          label: "Extra topics",
+          autogenerate: { directory: "guides/extra-topics" },
+        },
+        {
+          label: "Examples & Recipes",
+          autogenerate: { directory: "guides/examples-recipes" },
+        },
+      ],
+    }),
+  ],
 });
