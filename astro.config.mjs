ccbbabcc426b432521654bbf2abf4df6d0a5ec59
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2025-07-07 09:56:53 +0900

    Mighty!

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "My Docs",
+      title: "Mighty",
       social: [
         {
           icon: "github",
