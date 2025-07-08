43d682008790731fdad3b8a59a4f055b503b6f91
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2025-07-08 15:25:19 +0900

    Fix links and add validator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: "Mighty",
+      plugins: [starlightLinksValidator()],
       social: [
         {
           icon: "github",
